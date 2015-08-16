424db240ace0276aea8361ec937110c05ce5403f
Jason <373422363@qq.com>
2015-08-16 16:11:49 +0800

    提交项目框架

---
+++ hoishow/hoishow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hoishow
+//
+//  Created by Jason on 15/8/16.
+//  Copyright (c) 2015年 danche. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ hoishow/hoishow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hoishow
+//
+//  Created by Jason on 15/8/16.
+//  Copyright (c) 2015年 danche. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
